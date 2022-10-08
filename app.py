3a3e361f4f8c6d697118897e513d07d3b2e15718
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-10-08 20:22:47 +0200

    create racket 2

--- app.py
+++ app.py
@@ -10,10 +10,19 @@ wind.tracer(0) #Prevents the screen from updating
 racket1 = turtle.Turtle()
 racket1.speed(0)
 racket1.shape("square")
-racket1.color("red")
+racket1.color("green")
 racket1.shapesize(stretch_wid=5, stretch_len=1)
 racket1.penup()
 racket1.goto(-350, 0)
+
+#Racket2
+racket2 = turtle.Turtle()
+racket2.speed(0)
+racket2.shape("square")
+racket2.color("blue")
+racket2.shapesize(stretch_wid=5, stretch_len=1)
+racket2.penup()
+racket2.goto(350, 0)
 
 #main game loop
 while True:
@@ -21,5 +30,5 @@ while True:
 
 
 
-#Racket2
+
 
